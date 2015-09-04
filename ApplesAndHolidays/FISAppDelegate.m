79348bf72ca1f74fd227c23878dcc70ca2eca94a
sean-wantwave <sean@wantwave.com>
2015-09-04 12:43:25 -0700

    Completed methods for Holiday Supplies

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -16,10 +16,61 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+- (NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    
+    NSPredicate *appleFilter = [NSPredicate predicateWithFormat:@"self LIKE 'apple'"];
+    NSArray *appleOnlyArray = [fruits filteredArrayUsingPredicate:appleFilter];
+    
+    return appleOnlyArray;
+}
+
+- (NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    NSArray *holidaysInThisSeason = database[season];
+    
+    return holidaysInThisSeason;
+}
+
+- (NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    NSArray *holidaySuppliesNeeded = database[season][holiday];
+    
+    return holidaySuppliesNeeded;
+}
+
+- (BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    if ([[database[season] allKeys] containsObject:holiday]) {
+        return YES;
+    } else {
+        return NO;
+    }
+
+}
+
+- (BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    if ([database[season][holiday] containsObject:supply]) {
+        return YES;
+    } else {
+        return NO;
+    }
+    
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    database[season][holiday] = @[];
+    
+    return database;
+}
+
+- (NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    [database[season][holiday] addObject:supply];
+    
+    return database;
+}
 
 @end
