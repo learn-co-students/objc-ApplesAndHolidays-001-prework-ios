3efb2a719cb2a73f1a7a40e39685a83872a96138
Dnld <steinert.donald@gmail.com>
2015-12-16 10:45:09 -0500

    adds basic method skeletons

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -17,4 +17,40 @@
  
  */
 
+-(NSArray *)pickApplesFromFruits:(NSArray *)fruits
+{
+    return nil;
+}
+
+-(NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    return Nil;
+}
+
+- (NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    return nil;
+}
+
+- (BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    return nil;
+}
+
+- (BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    return nil;
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday toSeason :(NSString *)season inDatabase:(NSDictionary *)databse
+{
+    return nil;
+}
+
+- (NSDictionary *)addSupply:(NSDictionary *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)databse
+{
+    return nil;
+}
+
+
 @end
