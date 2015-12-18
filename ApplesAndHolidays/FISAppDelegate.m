9a2ce9178b1fcdff62298781a4ca720a11b0aab1
Shena Yoshida <sheyoshida@gmail.com>
2015-12-18 12:34:29 -0500

    set up methods

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,10 +11,61 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    
+    NSPredicate *applePredicate = [ NSPredicate predicateWithFormat:@"apples"];
+    NSArray *apples = [fruits filteredArrayUsingPredicate:applePredicate];
+    
+    for (NSArray *apple in apples) {
+        
+    }
+
+        return nil;
+}
+
+- (NSArray *)holidaysInSeason:(NSString *)season
+                   inDatabase:(NSDictionary *)database {
+    
+    return nil;
+}
+
+- (NSArray *)suppliesInHoliday:(NSString *)holiday
+                      inSeason:(NSString *)season
+                    inDatabase:(NSDictionary *)database {
+    
+    return nil;
+    
+}
+
+- (BOOL)holiday:(NSString *)holiday
+     isInSeason:(NSString *)season
+     inDatabase:(NSDictionary *)database {
+    
+    return YES;
+}
+
+- (BOOL)supply:(NSString *)supply
+   isInHoliday:(NSString *)holiday
+      inSeason:(NSString *)season
+    inDatabase:(NSDictionary *)database {
+    
+    return YES;
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday
+                    toSeason:(NSString *)season
+                  inDatabase:(NSDictionary *)database {
+    
+    return nil;
+}
+
+- (NSDictionary *)addSupply:(NSString *)supply
+                  toHoliday:(NSString *)holiday
+                   inSeason:(NSString *)season
+                 inDatabase:(NSDictionary *)database {
+    
+    return nil;
+}
+
 
 @end
