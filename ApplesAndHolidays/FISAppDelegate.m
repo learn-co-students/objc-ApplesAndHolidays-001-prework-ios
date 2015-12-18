780375262221735b20e16efcaf45125dce185e67
Shena Yoshida <sheyoshida@gmail.com>
2015-12-18 14:44:55 -0500

    added notes

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,6 +11,7 @@
     return YES;
 }
 
+//return an NSArray of all of the occurrences of the string @"apple" in the argument array fruits
 - (NSArray *)pickApplesFromFruits:(NSArray *)fruits {
     
     NSPredicate *applePredicate = [NSPredicate predicateWithFormat:@"self CONTAINS 'apple'"];
@@ -18,6 +19,7 @@
     return appleArray;
 }
 
+//return an array of all of the "holiday" keys in the sub-dictionary associated with the submitted "season" argument.
 - (NSArray *)holidaysInSeason:(NSString *)season
                    inDatabase:(NSDictionary *)database {
     
@@ -25,6 +27,7 @@
     return keysForSeason;
 }
 
+//return the mutable array of all of the supplies for the submitted "holiday" in the submitted "season".
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season
                     inDatabase:(NSDictionary *)database {
@@ -33,9 +36,9 @@
     partySupplies = database[season][holiday];
     
     return partySupplies;
-    
 }
 
+//return whether or not the submitted "season" contains a key that matches the "holiday" argument.
 - (BOOL)holiday:(NSString *)holiday
      isInSeason:(NSString *)season
      inDatabase:(NSDictionary *)database {
@@ -48,11 +51,11 @@
     return NO;
 }
 
+//return whether or not the submitted "holiday" in the submitted "season" contains in its array the submitted "supply" string.
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season
     inDatabase:(NSDictionary *)database {
-    
     
     for (NSString *item in database[season][holiday]) {
         if ([item isEqualToString:supply]) {
@@ -62,6 +65,7 @@
     return NO;
 }
 
+//create a new key-value pair in the submitted "season" key's sub-dictionary that uses the submitted "holiday" as the key and sets up an empty mutable array as its value.
 - (NSDictionary *)addHoliday:(NSString *)holiday
                     toSeason:(NSString *)season
                   inDatabase:(NSDictionary *)database {
@@ -71,13 +75,14 @@
     return database;
 }
 
+//insert the submitted "supply" argument into the mutable array of the submitted "holiday" key in the sub-dictionary of the submitted "season" key.
 - (NSDictionary *)addSupply:(NSString *)supply
                   toHoliday:(NSString *)holiday
                    inSeason:(NSString *)season
                  inDatabase:(NSDictionary *)database {
     
     [database[season][holiday] addObject:supply];
-    
+
     return database;
 }
 
