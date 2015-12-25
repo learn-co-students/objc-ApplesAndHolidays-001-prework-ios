cbb2b2aa685f3c699fc0846c66a36853b4e40910
dmead28 <dmead28@gmail.com>
2015-12-25 00:52:35 -0500

    Complete: dmead

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,10 +11,43 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+-(NSArray*)pickApplesFromFruits:(NSArray*)fruits {
+    NSPredicate *appleOccurances = [NSPredicate predicateWithFormat:@"self CONTAINS 'apple'"];
+    
+    return [fruits filteredArrayUsingPredicate:appleOccurances];
+}
+
+-(NSArray*)holidaysInSeason:(NSString*)season inDatabase:(NSDictionary*)database {
+    return [database[season] allKeys];
+}
+
+-(NSArray*)suppliesInHoliday:(NSString*)holiday inSeason:(NSString*)season inDatabase:(NSDictionary*)database {
+    return database[season][holiday];
+}
+
+-(BOOL)holiday:(NSString*)holiday isInSeason:(NSString*)season inDatabase:(NSDictionary*)database {
+    NSPredicate *holidayPredicate = [NSPredicate predicateWithFormat:@"self CONTAINS %@",holiday];
+    NSArray *filteredArray = [[self holidaysInSeason:season
+                                          inDatabase:database] filteredArrayUsingPredicate:holidayPredicate];
+    return [filteredArray count];
+}
+
+-(BOOL)supply:(NSString*)supply isInHoliday:(NSString*)holiday inSeason:(NSString*)season inDatabase:(NSDictionary*)database {
+    NSPredicate *supplyPredicate = [NSPredicate predicateWithFormat:@"self CONTAINS %@",supply];
+    NSArray *filteredArray = [[self suppliesInHoliday:holiday
+                                             inSeason:season
+                                           inDatabase:database] filteredArrayUsingPredicate:supplyPredicate];
+    return [filteredArray count];
+}
+
+-(NSDictionary*)addHoliday:(NSString*)holiday toSeason:(NSString*)season inDatabase:(NSDictionary*)database {
+    database[season][holiday] = [[NSMutableArray alloc] init];
+    return database;
+}
+
+-(NSDictionary*)addSupply:(NSString*)supply toHoliday:(NSString*)holiday inSeason:(NSString*)season inDatabase:(NSDictionary*)database {
+    [database[season][holiday] addObject:supply];
+    return database;
+}
 
 @end
