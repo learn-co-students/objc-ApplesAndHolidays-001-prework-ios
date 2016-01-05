adfced7ac7d7291c80db76d2b443c1a3db466acf
magfurulabeer <magfurulabeer@gmail.com>
2016-01-04 21:49:00 -0500

    Add first method

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -17,4 +17,18 @@
  
  */
 
+-(NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    NSPredicate *applePredicate = [NSPredicate predicateWithFormat:@"self LIKE 'apple' "];
+    NSArray *apples = [fruits filteredArrayUsingPredicate:applePredicate];
+    return apples;
+}
+
+-(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    //NSPredicate *holidayPredicate = [NSPredicate predicateWithFormat:@"self LIKE '%@' ", holiday];
+    //NSArray *holidays = [database[season] filteredArrayUsingPredicate:holidayPredicate];
+    NSLog(@" %@ ", database[season]);
+    //return holidays
+    return YES;
+}
+
 @end
