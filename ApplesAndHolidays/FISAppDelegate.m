f4fa517a36166a85cd3a40a2a4f7d3eaf15f3cde
magfurulabeer <magfurulabeer@gmail.com>
2016-01-05 10:33:34 -0500

    Complete project

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -23,12 +23,34 @@
     return apples;
 }
 
+-(NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSArray *holidays = [database[season] allKeys];
+    return holidays;
+}
+
+-(NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return database[season][holiday];
+}
+
 -(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
-    //NSPredicate *holidayPredicate = [NSPredicate predicateWithFormat:@"self LIKE '%@' ", holiday];
-    //NSArray *holidays = [database[season] filteredArrayUsingPredicate:holidayPredicate];
-    NSLog(@" %@ ", database[season]);
-    //return holidays
-    return YES;
+    NSArray *holidays = [self holidaysInSeason:season inDatabase:database];
+    return [holidays containsObject:holiday];
+}
+
+-(BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return [database[season][holiday] containsObject:supply];
+}
+
+-(NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableDictionary *targetSeason = database[season];
+    [targetSeason setObject:@[] forKey:holiday];
+    return database;
+}
+
+-(NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableArray *targetHoliday = database[season][holiday];
+    [targetHoliday addObject:supply];
+    return database;
 }
 
 @end
