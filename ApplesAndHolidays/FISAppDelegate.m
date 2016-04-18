f2a7f82dae556860ecede528ed25c365df086d7d
dochonglo <ismaelbarry777@hotmail.com>
2016-04-18 19:44:04 -0400

    Lab 17: Nesting and Filtering Practice is complete.

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -17,4 +17,60 @@
  
  */
 
+
+- (NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    
+    NSPredicate *applePredicate = [NSPredicate predicateWithFormat: @"self CONTAINS 'apple'"];
+    NSMutableArray *returnArray = [[fruits filteredArrayUsingPredicate:applePredicate] mutableCopy];
+    
+    return returnArray;
+}
+
+// return an array of all of the "holiday" keys in the sub-dictionary associated with the submitted "season" argument.
+- (NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+   
+    NSArray *returnArray = [database[season] allKeys];
+    return returnArray;
+     
+}
+
+// return the mutable array of all of the supplies for the submitted "holiday" in the submitted "season".
+- (NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    NSMutableArray *returnArray = database[season][holiday];
+    return returnArray;
+
+}
+
+// return whether or not the submitted "season" contains a key that matches the "holiday" argument.
+- (BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    NSMutableArray *returnArray = database[season][holiday];
+    [returnArray containsObject:holiday];
+    return returnArray;
+}
+
+// return whether or not the submitted "holiday" in the submitted "season" contains in its array the submitted "supply" string.
+- (BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    NSMutableArray *returnArray = database[season][holiday];
+    [returnArray containsObject:supply];
+    return returnArray;
+    
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    NSMutableArray *newArray = [[NSMutableArray alloc] init];
+    [database[season] setValue:newArray forKey:holiday];
+    return database;
+}
+
+// to insert the submitted "supply" argument into the mutable array of the submitted "holiday" key in the sub-dictionary of the submitted "season" key.
+- (NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    
+    [database[season][holiday] addObject:supply];
+    return database;
+}
+
 @end
